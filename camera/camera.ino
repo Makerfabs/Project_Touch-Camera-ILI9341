8cf2936d92f95e80f4fadfc5277948b368632551
195cn <2259761058@qq.com>
2020-09-29 11:15:31 +0800

    add test code

---
+++ camera/camera.ino
@@ -0,0 +1,232 @@
+#include <SPI.h>
+#include "Adafruit_GFX.h"
+#include "Adafruit_ILI9341.h"
+#include "Adafruit_STMPE610.h"
+#include "SD.h"
+#include "FS.h"
+
+#include "esp_camera.h"
+#define CAMERA_MODEL_MAKERFABS
+#include "camera_pins.h"
+
+#define ARRAY_LENGTH 320 * 240 * 3
+#define SCRENN_ROTATION 3
+
+#define SDA 26
+#define SCL 27
+
+//SPI
+#define SPI_MOSI 13
+#define SPI_MISO 12
+#define SPI_SCK 14
+
+//SD Card
+#define SD_CS 4
+
+//Touch Screen
+#define STMPE_CS 2
+
+//TFT
+#define TFT_CS 15
+#define TFT_DC 33
+#define TFT_LED -1 //1//-1
+#define TFT_RST -1 //3//-1
+
+//SPI control
+#define SPI_ON_TFT digitalWrite(TFT_CS, LOW)
+#define SPI_OFF_TFT digitalWrite(TFT_CS, HIGH)
+#define SPI_ON_SD digitalWrite(SD_CS, LOW)
+#define SPI_OFF_SD digitalWrite(SD_CS, HIGH)
+#define STMPE_ON digitalWrite(STMPE_CS, LOW)
+#define STMPE_OFF digitalWrite(STMPE_CS, HIGH)
+
+Adafruit_ILI9341 tft = Adafruit_ILI9341(TFT_CS, TFT_DC, TFT_RST);
+Adafruit_STMPE610 touch = Adafruit_STMPE610(STMPE_CS);
+
+void setup()
+{
+
+    esp32_init();
+    camera_init();
+}
+
+void loop()
+{
+    camera_fb_t *fb = NULL;
+
+    fb = esp_camera_fb_get();
+
+    drawRGBBitmap(fb->buf, fb->width, fb->height);
+
+    esp_camera_fb_return(fb);
+}
+
+//ILI9488 init and SD card init
+void esp32_init()
+{
+    Serial.begin(115200);
+    Serial.println("ILI9488 Test!");
+
+    //I2C init
+    Wire.begin(SDA, SCL);
+
+    //SPI init
+    pinMode(SD_CS, OUTPUT);
+    pinMode(TFT_CS, OUTPUT);
+    pinMode(STMPE_CS, OUTPUT);
+    SPI_OFF_SD;
+    SPI_OFF_TFT;
+    STMPE_OFF;
+    SPI.begin(SPI_SCK, SPI_MISO, SPI_MOSI);
+
+    SPI_ON_SD;
+    //SD(SPI) init
+    if (!SD.begin(SD_CS, SPI, 80000000))
+    {
+        Serial.println("Card Mount Failed");
+        while (1)
+            ;
+    }
+    else
+    {
+        Serial.println("Card Mount Successed");
+    }
+    //sd_test();
+    SPI_OFF_SD;
+
+    Serial.println("SD init over.");
+
+    //TFT(SPI) init
+    SPI_ON_TFT;
+    tft.begin();
+    tft.setRotation(3);
+    tft.fillScreen(ILI9341_BLACK);
+    tft.setTextColor(ILI9341_RED);
+    tft.setTextSize(4);
+    tft.println("TOUCH TO START TEST");
+    tft.setRotation(1);
+    SPI_OFF_TFT;
+
+    STMPE_ON;
+    if (!touch.begin())
+    {
+        Serial.println("STMPE not found!");
+        while (1)
+            ;
+    }
+    while (1)
+    {
+        if (touch.touched())
+        {
+            Serial.println("Touched");
+            break;
+        }
+
+        delay(100);
+    }
+    STMPE_OFF;
+
+    Serial.println("TFT init over.");
+    print_img(SD, "/logo.bmp");
+}
+
+//Camera setting
+void camera_init()
+{
+    //camera config
+    camera_config_t config;
+    config.ledc_channel = LEDC_CHANNEL_0;
+    config.ledc_timer = LEDC_TIMER_0;
+    config.pin_d0 = Y2_GPIO_NUM;
+    config.pin_d1 = Y3_GPIO_NUM;
+    config.pin_d2 = Y4_GPIO_NUM;
+    config.pin_d3 = Y5_GPIO_NUM;
+    config.pin_d4 = Y6_GPIO_NUM;
+    config.pin_d5 = Y7_GPIO_NUM;
+    config.pin_d6 = Y8_GPIO_NUM;
+    config.pin_d7 = Y9_GPIO_NUM;
+    config.pin_xclk = XCLK_GPIO_NUM;
+    config.pin_pclk = PCLK_GPIO_NUM;
+    config.pin_vsync = VSYNC_GPIO_NUM;
+    config.pin_href = HREF_GPIO_NUM;
+    config.pin_sscb_sda = SIOD_GPIO_NUM;
+    config.pin_sscb_scl = SIOC_GPIO_NUM;
+    config.pin_pwdn = PWDN_GPIO_NUM;
+    config.pin_reset = RESET_GPIO_NUM;
+    config.xclk_freq_hz = 20000000;
+
+    config.pixel_format = PIXFORMAT_RGB565;
+    config.frame_size = FRAMESIZE_QVGA;
+    config.jpeg_quality = 10;
+    config.fb_count = 1;
+
+    // camera init
+    esp_err_t err = esp_camera_init(&config);
+    if (err != ESP_OK)
+    {
+        Serial.printf("Camera init failed with error 0x%x", err);
+        while (1)
+            ;
+    }
+
+    sensor_t *s = esp_camera_sensor_get();
+    //initial sensors are flipped vertically and colors are a bit saturated
+    if (s->id.PID == OV2640_PID)
+    {
+        s->set_vflip(s, 0);      //flip it back
+        s->set_brightness(s, 0); //up the blightness just a bit
+        s->set_saturation(s, 1); //lower the saturation
+    }
+    //drop down frame size for higher initial frame rate
+    s->set_framesize(s, FRAMESIZE_QVGA);
+}
+
+void drawRGBBitmap(uint8_t *bitmap, int16_t w, int16_t h)
+{
+    SPI_ON_TFT;
+    tft.startWrite();
+    for (int16_t j = 0; j < h; j++)
+    {
+        for (int16_t i = 0; i < w; i++)
+        {
+            uint16_t temp = bitmap[j * w * 2 + 2 * i] * 256 + bitmap[j * w * 2 + 2 * i + 1];
+            tft.writePixel(i, j, temp);
+        }
+    }
+    tft.endWrite();
+    SPI_OFF_TFT;
+}
+
+//From SD
+int print_img(fs::FS &fs, String filename)
+{
+    SPI_ON_SD;
+    File f = fs.open(filename);
+    if (!f)
+    {
+        Serial.println("Failed to open file for reading");
+        return 0;
+    }
+
+    f.seek(54);
+    int X = 320;
+    int Y = 240;
+    uint8_t RGB[3 * X];
+    for (int row = 0; row < Y; row++)
+    {
+        f.seek(54 + 3 * X * row);
+        f.read(RGB, 3 * X);
+        SPI_OFF_SD;
+        SPI_ON_TFT;
+        for (int col = 0; col < X; col++)
+        {
+            tft.drawPixel(col, row, tft.color565(RGB[col * 3 + 2], RGB[col * 3 + 1], RGB[col * 3]));
+        }
+        SPI_OFF_TFT;
+        SPI_ON_SD;
+    }
+
+    f.close();
+    SPI_OFF_SD;
+    return 0;
+}
