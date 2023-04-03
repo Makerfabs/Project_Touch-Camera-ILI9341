85d5d5b5db971b29fc930b38a3dc31240d17eac2
一九五 <2259761058@qq.com>
2023-04-03 10:23:28 +0800

    Add New Version LGFX demo

---
+++ example/LGFX_demo/pin_set.h
@@ -0,0 +1,25 @@
+#ifndef PIN_SET_H
+#define PIN_SET_H
+
+#define SDA 26
+#define SCL 27
+
+//SPI
+#define SPI_MOSI 13
+#define SPI_MISO 12
+#define SPI_SCK 14
+
+//SD Card
+#define SD_CS 4
+
+//Touch Screen
+#define STMPE_CS 2
+
+//TFT
+#define TFT_CS 15
+#define TFT_DC 33
+#define TFT_LED -1 //1//-1
+#define TFT_RST -1 //3//-1
+
+
+#endif
